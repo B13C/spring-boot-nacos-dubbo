5230cc43acd2d075222c50c58779a6e797ed4ef3
枫叶思源 <britton@126.com>
2021-09-05 01:13:58 +0800

    重构

--- consumer/src/main/java/com/geoxus/controller/HelloController.java
+++ consumer/src/main/java/com/geoxus/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.geoxus.controller;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.geoxus.service.IOrderService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,17 @@ public class HelloController {
     @Resource
     private IOrderService orderService;
 
+    @NacosValue(value = "${my-name:123}", autoRefreshed = true)
+    private String name;
+
     @GetMapping("hello")
     public String hello(String userId) {
         System.out.println(orderService.initOrder(userId));
         return "Hello Controller";
+    }
+
+    @GetMapping("index")
+    public String index() {
+        return "Hello Controller , " + name;
     }
 }
