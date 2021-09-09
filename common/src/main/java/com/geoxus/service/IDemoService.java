7cd9e70182811ca706cc9d03f57c1abe08c3a9d1
chenzhongjian <britton@126.com>
2021-09-09 10:17:42 +0800

    功能完善

--- common/src/main/java/com/geoxus/service/IDemoService.java
+++ common/src/main/java/com/geoxus/service/IDemoService.java
@@ -2,4 +2,6 @@ package com.geoxus.service;
 
 public interface IDemoService {
     String demoSayHello(String name);
+
+    String hello(String name);
 }
--- provider-zk/src/main/java/com/geoxus/service/impl/DemoService.java
+++ provider-zk/src/main/java/com/geoxus/service/impl/DemoService.java
@@ -1,5 +1,6 @@
 package com.geoxus.service.impl;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.geoxus.service.IDemoService;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.stereotype.Service;
@@ -7,8 +8,16 @@ import org.springframework.stereotype.Service;
 @DubboService
 @Service
 public class DemoService implements IDemoService {
+    @NacosValue(value = "${my-name:jackkkkkkkk}" , autoRefreshed = true)
+    private String myName;
+
     @Override
     public String demoSayHello(String name) {
         return "Hi , ZK的注册中心! (●'◡'●)" + name;
+    }
+
+    @Override
+    public String hello(String name) {
+        return "我的hello方法 , ZK的注册中心！！！！！! (●'◡'●)" + name + " " + myName;
     }
 }
